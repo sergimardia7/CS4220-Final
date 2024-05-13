d669a993e07ad2b01f81e8eb3daf3c5b76561f1c
jessewrx <126747123+jessewrx@users.noreply.github.com>
2024-05-13 16:11:18 -0700

    Update db.js

--- services/db.js
+++ services/db.js
@@ -1,135 +1,57 @@
-import  dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 
-/**
- * ES6 module for interacting with MongoDB
- * @returns {Object} - Object containing functions to interact with MongoDB
- */
-const mongo = () => {
-    // Load the environment variables from the .env file
-    dotenv.config();
+dotenv.config();
 
-    const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-    const mongoURL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}/?retryWrites=true&w=majority&appName=Cluster0`;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const mongoURL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
 
-    let client;
-    let db;
+let client;
+let db;
 
-    /**
-     * Opens a connection to the MongoDB database
-     */
-    async function connect() {
-        try {
-            client = new MongoClient(mongoURL);
-            await client.connect();
-            db = client.db();
-
-            console.log('Opened connection to MongoDB');
-        } catch (err) {
-            console.error(err);
-        }
+export async function connect() {
+    try {
+        client = new MongoClient(mongoURL);
+        await client.connect();
+        db = client.db();
+        console.log('Opened connection to MongoDB');
+    } catch (err) {
+        console.error(err);
     }
+}
 
-    /**
-     * Closes the connection to the MongoDB database
-     */
-    async function close() {
-        try {
-            await client.close();
-
-            console.log('Closed connection to MongoDB');
-        } catch (err) {
-            console.error(err);
-        }
+export async function close() {
+    try {
+        await client.close();
+        console.log('Closed connection to MongoDB');
+    } catch (err) {
+        console.error(err);
     }
+}
 
-    /**
-     * Creates a new document in the specified collection
-     * @param {string} Brewerycheck - name of the collection
-     * @param {Object} data - data to be inserted into the collection
-     */
-    async function create(Brewerycheck, data) {
-        try {
-            const StatCheck = db.collection(Brewerycheck);
-            await StatCheck.insertOne(data);
-        } catch (err) {
-            console.error(err);
-        }
+export async function create(collectionName, data) {
+    try {
+        const collection = db.collection(collectionName);
+        await collection.insertOne(data);
+    } catch (err) {
+        console.error(err);
     }
+}
 
-    /**
-     * Finds documents in the specified collection
-     * @param {string} Brewerycheck - name of the collection
-     * @param {string} Id - identifier for filtering documents
-     * @returns {Promise<Document>} - a document or an array of ducments
-     */
-    async function find(Brewerycheck, Id) {
-        try {
-            const IdCheck = db.collection(Brewerycheck);
-
-            if (Id) {
-                return await IdCheck.find({ Id1: Id }).next();
-            } else {
-                return await IdCheck
-                    .find({})
-                    .limit(10)
-                    .sort({ _id: -1 })
-                    .toArray();
-            }
-        } catch (err) {
-            console.error(err);
-        }
+export async function find(collectionName, query = {}) {
+    try {
+        const collection = db.collection(collectionName);
+        return await collection.find(query).toArray();
+    } catch (err) {
+        console.error(err);
     }
-    /**
-     * Updates documents in the specified collection
-     * @param {string} Brewerycheck - name of the collection
-     * @param {string} Id - identifier for filtering documents
-     * @param {Object} data - the data to be updated
-     */
-    async function update(Brewerycheck, Id, data) {
-        try {
-            const IdCheck = db.collection(Brewerycheck);
-            await IdCheck.updateOne(
-                { Id1: Id },
-                { $set: data }
-            );
-        } catch (err) {
-            console.error("There is an error", err);
-        }
+}
+
+export async function update(collectionName, query, updateData) {
+    try {
+        const collection = db.collection(collectionName);
+        await collection.updateOne(query, { $set: updateData }, { upsert: true });
+    } catch (err) {
+        console.error(err);
     }
-
-
-    return {
-        connect,
-        close,
-        create,
-        find,
-        update
-    };
-};
-
-export default mongo();
-
-// updated db.js
-// import express from 'express';
-// import { searchByKeyword } from '../services/api.js';
-// import { create } from '../services/db.js';
-
-// const router = express.Router();
-
-// router.get('/', async (req, res) => {
-//     try {
-//         const { searchTerm } = req.query;
-//         const results = await searchByKeyword(searchTerm);
-//         // Process results and send response
-//         res.json({ results });
-
-//         // Update or create search history
-//         await create('search_history', { searchTerm, searchCount: results.length, lastSearched: new Date() });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
-// export default router;
+}
