10b4df761d1b06fff4953aafcffcd0eaa4dd32a0
jessewrx <126747123+jessewrx@users.noreply.github.com>
2024-05-13 16:22:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,19 @@
-const express = require('express');
-const axios = require('axios');
+const PORT = 8888;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const API_BASE_URL = 'https://api.openbrewerydb.org/breweries';
 
+//app.use(express.json());
 
-app.get('/breweries/:city', async (req, res) => {
-  try {
-    const city = req.params.city;
-    const response = await axios.get(`${API_BASE_URL}?by_city=${city}`);
-    const breweries = response.data;
-    res.json(breweries);
-  } catch (error) {
-    console.error('Error fetching breweries:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+app.get('/', (req, res) => {
+    res.send('Welcome to the Brewery Search App');
 });
 
+app.use('/search', searchRoutes);
+app.use('/:id/details', searchRoutes);
+app.use('/history', historyRoutes);
 
-app.get('/brewery/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    const response = await axios.get(`${API_BASE_URL}/${id}`);
-    const brewery = response.data;
-    res.json(brewery);
-  } catch (error) {
-    console.error('Error fetching brewery:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+app.listen(PORT, async () => {
+    await connect();
+    console.log(`Server is listening on port ${PORT}`);
 });
 
-// Start the server :)
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
